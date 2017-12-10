83e830b25a767364b63aed2162d60f4163ca793a
Marc Magon <ged_marc@hotmail.com>
2017-12-10 04:29:56 +0200

    Javadoc Updates

--- src/main/java/za/co/mmagon/jwebswing/undertow/UndertowJWebSwingHandlerExtension.java
+++ src/main/java/za/co/mmagon/jwebswing/undertow/UndertowJWebSwingHandlerExtension.java
@@ -6,20 +6,24 @@ import io.undertow.server.handlers.resource.ClassPathResourceManager;
 import io.undertow.servlet.ServletExtension;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.FilterInfo;
+import za.co.mmagon.logger.LogFactory;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.ServletContext;
+import java.util.logging.Logger;
 
 @SuppressWarnings("unused")
 public class UndertowJWebSwingHandlerExtension implements ServletExtension
 {
+	private static final Logger log = LogFactory.getLog("JwebSwingUndertow");
+
 	@Override
 	public void handleDeployment(DeploymentInfo deploymentInfo, ServletContext servletContext)
 	{
-		System.out.println("Registering JWebSwing in undertow");
-		deploymentInfo.addFilter(new FilterInfo("GuiceFilter",GuiceFilter.class));
+		log.config("Registering JWebSwing in undertow");
+		deploymentInfo.addFilter(new FilterInfo("GuiceFilter", GuiceFilter.class));
 		deploymentInfo.addFilterUrlMapping("GuiceFilter", "/*", DispatcherType.REQUEST);
-		deploymentInfo.setResourceManager(new ClassPathResourceManager(deploymentInfo.getClassLoader(),"META-INF/resources"));
+		deploymentInfo.setResourceManager(new ClassPathResourceManager(deploymentInfo.getClassLoader(), "META-INF/resources"));
 		GuiceContext.inject();
 	}
 }
