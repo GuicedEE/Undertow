c626d3b1d9e71e09985f83d98eb057e78800af2a
GedMarc <ged_marc@hotmail.com>
2018-07-19 00:43:41 +0200

    JDK 10 and Builds

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,5 +1,9 @@
+import com.jwebmp.undertow.UndertowJWebSwingHandlerExtension;
+
 module com.jwebmp.undertow
 {
+	exports com.jwebmp.undertow;
+
 	requires com.google.guice.extensions.servlet;
 	requires com.jwebmp.logmaster;
 	requires undertow.core;
@@ -7,4 +11,5 @@ module com.jwebmp.undertow
 	requires javax.servlet.api;
 	requires java.logging;
 
+	provides io.undertow.servlet.ServletExtension with UndertowJWebSwingHandlerExtension;
 }
