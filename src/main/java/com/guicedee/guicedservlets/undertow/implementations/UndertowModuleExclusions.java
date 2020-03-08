be796bfc4360d18bf4a09f181dc8abac83f3b41b
GedMarc <ged_marc@hotmial.com>
2020-03-08 17:00:30 +0200

    Remove Jar syntax as JDK 8 is no longer supported

--- src/main/java/com/guicedee/guicedservlets/undertow/implementations/UndertowModuleExclusions.java
+++ src/main/java/com/guicedee/guicedservlets/undertow/implementations/UndertowModuleExclusions.java
@@ -1,6 +1,5 @@
 package com.guicedee.guicedservlets.undertow.implementations;
 
-import com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions;
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
 import javax.validation.constraints.NotNull;
@@ -8,22 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class UndertowModuleExclusions
-		implements IGuiceScanModuleExclusions<UndertowModuleExclusions>,
-				           IGuiceScanJarExclusions<UndertowModuleExclusions>
+		implements IGuiceScanModuleExclusions<UndertowModuleExclusions>
 {
-	@Override
-	public @NotNull Set<String> excludeJars()
-	{
-		Set<String> strings = new HashSet<>();
-		strings.add("jwebmp-undertow-*");
-		strings.add("uadetector-core-*");
-		strings.add("uadetector-servlet-*");
-		strings.add("uadetector-websockets-*");
-		strings.add("xnio-api-*");
-		strings.add("xnio-nio-*");
-		return strings;
-	}
-
 	@Override
 	public @NotNull Set<String> excludeModules()
 	{
