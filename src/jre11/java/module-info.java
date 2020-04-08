cd64eeeec88e901a14dfc7126786a254caf93bc7
GedMarc <marc.magon@gmail.com>
2020-04-08 18:08:21 +0200

    Hibernate to 5.4.14

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -3,28 +3,21 @@ module com.guicedee.guicedservlets.undertow
 	exports com.guicedee.guicedservlets.undertow;
 	exports com.guicedee.guicedservlets.undertow.services;
 
-	requires com.google.guice.extensions.servlet;
+	requires transitive com.guicedee.guicedservlets.websockets;
+
 
 	requires transitive undertow.core;
 	requires transitive undertow.servlet;
-	requires transitive javax.servlet.api;
+
 	requires java.logging;
 	requires xnio.api;
+
 	requires undertow.websockets.jsr;
-	requires com.google.common;
-	requires com.google.guice;
-	requires javax.inject;
 
-	requires transitive com.guicedee.logmaster;
-	requires transitive com.guicedee.guicedservlets.websockets;
 
-	requires transitive com.guicedee.guicedinjection;
-	//requires transitive com.guicedee.core;
-	requires transitive com.guicedee.guicedservlets;
 	requires transitive java.validation;
 
 	requires transitive jdk.unsupported;
-	requires io.github.classgraph;
 
 	provides io.undertow.servlet.ServletExtension with com.guicedee.guicedservlets.undertow.UndertowGuicedHandlerExtension;
 	provides com.guicedee.guicedservlets.websockets.services.IWebSocketPreConfiguration with com.guicedee.guicedservlets.undertow.GuicedUndertowWebSocketConfiguration;
